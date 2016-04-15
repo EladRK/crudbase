d6a44655714cf2f60885a91fe74976963e8d2e1f
Elad Katz <eladkt@gmail.com>
2016-04-15 14:18:40 +0300

    removed mongoose plugins for now

--- model/article.js
+++ model/article.js
@@ -1,18 +1,18 @@
 'use strict';
 
-const ObjectIdType = require('mongoose').Schema.Types.ObjectId;
+const objectId = require('mongoose').Schema.Types.ObjectId;
 const seoSchema = require('./seo');
 
 module.exports = {
-    segmentId: ObjectIdType,
-    authorId: ObjectIdType,
+    segmentId: objectId,
+    authorId: objectId,
     title: String,
     brief: String,
     body: String,
     seo: seoSchema,
-    verticalId: ObjectIdType,
+    verticalId: objectId,
     name: String,
-    tagId: [{type: ObjectIdType}],
+    tagId: [{type: objectId}],
     thumb: {},
     createDate: Date,
     updateDate: Date
