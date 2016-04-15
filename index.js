4e8e3584e05e0bec2bcbc289372e0e46ba1ccfcd
Elad Katz <eladkt@gmail.com>
2016-04-15 09:52:55 +0300

    some more refactoring

--- index.js
+++ index.js
@@ -7,16 +7,19 @@ var colors = require('colors/safe');
 
 
 app.use(bodyParser.json());
+
+
+var db = require('./lib/db')
+var createCrudRoutes = require('./lib/crud-routes');
+
+var crudRoutes = createCrudRoutes(db);
+app.use('/', crudRoutes);
+
+
 app.use(express.static('public'));
 app.use('/angular', express.static(__dirname + '/node_modules/angular/'));
 app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist/css/'));
 app.use('/node_modules/ng-admin/build/', express.static(__dirname + '/node_modules/ng-admin/build/'));
-
-
-var crudService = require('./services/crud');
-
-var router = crudService.createCrudRoutes();
-app.use('/', router);
 
 
 mongoose.connect('mongodb://localhost/umb-simple');
